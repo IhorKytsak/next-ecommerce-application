3ddd016514da9888bd3eb0220ce0619054f25e52
Ihor Kytsak <kitsak735@gmail.com>
2023-04-12 22:08:31 +0300

    ptoject configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
+const { withFrameworkConfig } = require('./framework/common/config');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    framework: {
+      name: 'shopify_local',
+    },
+  },
 
-module.exports = nextConfig
+  i18n: {
+    locales: ['en-US', 'ua'],
+    defaultLocale: 'en-US',
+  },
+};
+
+module.exports = withFrameworkConfig(nextConfig);
