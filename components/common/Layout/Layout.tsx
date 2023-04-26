2b6e1daf6cbbd4806f00dd5fe8c33a0a89b3af2b
Ihor Kytsak <kitsak735@gmail.com>
2023-04-26 16:52:39 +0300

    add Sidebars

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import type { FC, ReactNode } from 'react';
 import { Footer, Navbar } from '@components/common';
+import { Sidebar } from '@components/ui';
+import { CartSidebar } from '@components/cart';
 import s from './Layout.module.css';
 
 export interface LayoutProps {
@@ -10,6 +12,9 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={s.root}>
       <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
       <main className='fit'>{children}</main>
       <Footer />
     </div>
