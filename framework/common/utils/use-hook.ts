3eb08f993c20644cf12cfa3175e53edab5961722
Ihor Kytsak <kitsak735@gmail.com>
2023-05-12 11:38:52 +0300

    call fetcher in use data

--- framework/common/utils/use-hook.ts
+++ framework/common/utils/use-hook.ts
@@ -1,12 +1,12 @@
 import { useApiProvider } from '@common';
+import { ApiFetcher } from '@common/types/api';
 import { ApiHooks } from '@common/types/hooks';
 import { MutationHook } from '@common/types/hooks';
-
+import { useState } from 'react';
 export const useHook = (fn: (apiHooks: ApiHooks) => MutationHook) => {
   const { hooks } = useApiProvider();
   return fn(hooks);
 };
-
 export const useMutationHook = (hook: MutationHook) => {
   const { fetcher } = useApiProvider();
   return hook.useHook({
@@ -16,6 +16,41 @@ export const useMutationHook = (hook: MutationHook) => {
         fetch: fetcher,
         options: hook.fetcherOptions,
       });
+    },
+  });
+};
+
+const useData = (hook: any, fetcher: ApiFetcher) => {
+  const [data, setData] = useState(null);
+
+  const hookFetcher = async () => {
+    try {
+      return await hook.fetcher({
+        fetch: fetcher,
+        options: hook.fetchOptions,
+      });
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  if (!data) {
+    hookFetcher().then((data) => {
+      setData(data);
+    });
+  }
+
+  return data;
+};
+
+// cache data first if possible
+export const useSWRHook = (hook: any) => {
+  const { fetcher } = useApiProvider();
+
+  return hook.useHook({
+    useData() {
+      const data = useData(hook, fetcher);
+      return data;
     },
   });
 };
