9c57f2bd49aac024fe5cc6cdfa230ceb3fb24fd2
Ihor Kytsak <kitsak735@gmail.com>
2023-06-06 11:21:44 +0300

    loading and button UX improvements

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -1,16 +1,26 @@
-import { ButtonHTMLAttributes, FC, ReactNode } from 'react';
-import cn from 'classnames';
+import {
+  ButtonHTMLAttributes,
+  ComponentType,
+  FC,
+  HTMLAttributes,
+  ReactNode,
+} from 'react';
+import { LoadingDots } from '@components/ui';
 import s from './Button.module.css';
+import cn from 'classnames';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode | ReactNode[];
   isLoading?: boolean;
+  Component?: string | ComponentType<HTMLAttributes<HTMLElement>>;
+  href?: string;
 }
 
 const Button: FC<Props> = ({
   children,
   className,
   isLoading = false,
+  Component = 'button',
   ...rest
 }) => {
   const rootClassName = cn(s.root, className, {
@@ -18,10 +28,15 @@ const Button: FC<Props> = ({
   });
 
   return (
-    <button className={rootClassName} type='button' {...rest}>
+    <Component className={rootClassName} type='button' {...rest}>
       {children}
-      {isLoading && <div>Loading...</div>}
-    </button>
+      {isLoading && (
+        <i className='pl-2 m-0 flex'>
+          <LoadingDots />
+        </i>
+      )}
+    </Component>
   );
 };
+
 export default Button;
